6f9e9acf235c69cee50bfecd0bd56581f92c344a
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-08-30 11:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+let modal = document.getElementById("modal-content")
+let button = document.getElementById("openModal")
+let closeButton = document.querySelector(".close-modal")
+let modalClass = document.querySelector("modal")
+
+modal.style.display = "none";
+
+button.onclick = ()=>{
+	modal.style.display = "flex";
+	modal.style.justify
+}
+closeButton.onclick = () =>{
+	modal.style.display = "none";
+}
+
+ window.onclick = (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+      }
+    };
